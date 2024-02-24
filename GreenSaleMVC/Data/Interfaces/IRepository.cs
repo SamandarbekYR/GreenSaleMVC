f06f60b700b716cd83337a8bb43ed8d272707a41
SamandarbekYr <SamandarbekYR@gmail.com>
2024-02-24 11:57:31 +0500

    DATA: IRepository and Repository Created

---
+++ GreenSaleMVC/Data/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using GreenSaleMVC.Data.Entities;
+
+namespace GreenSaleMVC.Data.Interfaces
+{
+    public interface IRepository <TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll();
+        TEntity? GetById(Guid id);
+        int Remove(Guid id);
+        int Update(TEntity entity, Guid id);
+        ValueTask<int> Add(TEntity entity);
+    }
+}
---
+++ GreenSaleMVC/Data/Repositories/Repository.cs
@@ -0,0 +1,61 @@
+﻿using GreenSaleMVC.Data.Entities;
+using GreenSaleMVC.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenSaleMVC.Data.Repositories
+{
+    public class Repository<TEntity>(AppDbContext appDb)
+        : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        public AppDbContext _dbContext = appDb;
+        public DbSet<TEntity> _dbSet = appDb.Set<TEntity>();
+        
+        public async ValueTask<int> Add(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public TEntity? GetById(Guid id)
+        {
+            TEntity? entity = _dbSet.FirstOrDefault(x => x.Id == id);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            return entity;
+        }
+
+        public int Remove(Guid id)
+        {
+            TEntity? entity = GetById(id);
+
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                return _dbContext.SaveChanges();
+            }
+
+            return 0;
+        }
+
+        public int Update(TEntity entity, Guid id)
+        {
+            TEntity? model = GetById(id);
+
+            if (model != null)
+            {
+                _dbSet.Update(entity);
+                return _dbContext.SaveChanges();
+            }
+
+            return 0;
+        }
+    }
+}
